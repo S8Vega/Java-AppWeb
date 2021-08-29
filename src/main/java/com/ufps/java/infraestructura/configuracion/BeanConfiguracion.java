902b76c295aad736b38b68ecd8e7c035c9b3847c
Sebas <bsebastianvg18@gmail.com>
2021-08-29 16:40:04 -0500

    agregar convertir de docx a pdf

--- src/main/java/com/ufps/java/infraestructura/configuracion/BeanConfiguracion.java
+++ src/main/java/com/ufps/java/infraestructura/configuracion/BeanConfiguracion.java
@@ -1,6 +1,7 @@
 package com.ufps.java.infraestructura.configuracion;
 
 import com.ufps.java.dominio.usecase.ConvertirDocxOdtUseCaseImpl;
+import com.ufps.java.dominio.usecase.ConvertirDocxPdfUseCaseImpl;
 import com.ufps.java.dominio.usecase.ConvertirUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,11 @@ public class BeanConfiguracion {
     @Bean(name = "DOCX_ODT")
     ConvertirUseCase getConvertirDocxOdtUseCaseImpl() {
         return new ConvertirDocxOdtUseCaseImpl();
+    }
+
+    @Bean(name = "DOCX_PDF")
+    ConvertirUseCase getConvertirDocxPdfUseCaseImpl() {
+        return new ConvertirDocxPdfUseCaseImpl();
     }
 
 }
