43ab17bc34df2fd851858f524c7bc55d05fd897f
Sebas <bsebastianvg18@gmail.com>
2021-08-30 18:07:07 -0500

    agregar acceso desde cualquier ubicacion

--- src/test/java/com/ufps/java/infraestructura/JavaAppWebApplicationTests.java
+++ src/test/java/com/ufps/java/infraestructura/JavaAppWebApplicationTests.java
@@ -9,15 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JavaAppWebApplicationTests {
 
-    private static final String DIR = "src/main/resources/files/";
-
     @Test
-    void docx() throws Exception {
-        Document wpd = new Document(DIR + "nombre.docx");
-
-        wpd.save(DIR + "output.odt", SaveFormat.ODT);
-
-        wpd.save(DIR + "output.pdf", SaveFormat.PDF);
+    void ejecutar() {
     }
 
 }
